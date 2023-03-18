946cd158db20ef95d29c028db8655dd726ec4700
Eduardo Pousa <92334531+epousa@users.noreply.github.com>
2023-03-18 21:39:04 +0000

    Update Consumer_2

--- cenarios-teste/monitoring-stack/config/kafka/test-consumer/src/Consumer.java
+++ cenarios-teste/monitoring-stack/config/kafka/test-consumer/src/Consumer.java
@@ -1,210 +1,143 @@
-/*******************************************************************************
- * This file is part of OpenNMS(R).
- *
- * Copyright (C) 2021 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2021 The OpenNMS Group, Inc.
- *
- * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
- *
- * OpenNMS(R) is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- *
- * OpenNMS(R) is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with OpenNMS(R).  If not, see:
- *      http://www.gnu.org/licenses/
- *
- * For more information contact:
- *     OpenNMS(R) Licensing <license@opennms.org>
- *     http://www.opennms.org/
- *     http://www.opennms.com/
- *******************************************************************************/
+package Main;
 
- package Main;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+//import org.apache.kafka.clients.consumer.ConsumerRecord;
+// import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+//import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
- import java.io.IOException;
- import java.util.ArrayList;
- import java.util.Arrays;
- import java.util.Dictionary;
- import java.util.Enumeration;
- import java.util.List;
- import java.util.Properties;
- import java.util.concurrent.ExecutorService;
- import java.util.concurrent.Executors;
- import java.util.concurrent.ThreadFactory;
- import java.util.concurrent.atomic.AtomicBoolean;
- 
- import org.apache.kafka.clients.consumer.ConsumerConfig;
- import org.apache.kafka.clients.consumer.ConsumerRecord;
- import org.apache.kafka.clients.consumer.ConsumerRecords;
- import org.apache.kafka.clients.consumer.KafkaConsumer;
- import org.apache.kafka.common.serialization.ByteArrayDeserializer;
- import org.apache.kafka.common.serialization.StringDeserializer;
- import org.opennms.core.ipc.common.kafka.Utils;
- import org.opennms.core.utils.SystemInfoUtils;
- import org.opennms.features.kafka.consumer.events.EventsMapper;
- import org.opennms.features.kafka.consumer.events.EventsProto;
- import org.opennms.netmgt.events.api.EventForwarder;
- import org.opennms.netmgt.xml.event.Event;
- import org.opennms.netmgt.xml.event.Log;
- import org.osgi.service.cm.ConfigurationAdmin;
- import org.slf4j.Logger;
- import org.slf4j.LoggerFactory;
- 
- import com.google.common.base.Strings;
- import com.google.common.util.concurrent.ThreadFactoryBuilder;
- import com.google.protobuf.InvalidProtocolBufferException;
- 
- public class OpenNMSKafkaConsumer {
- 
-     private static final Logger LOG = LoggerFactory.getLogger(OpenNMSKafkaConsumer.class);
- 
-     private final ConfigurationAdmin configAdmin;
- 
-     private final EventForwarder eventForwarder;
- 
-     private String eventsTopic;
- 
-     private final AtomicBoolean closed = new AtomicBoolean(false);
- 
-     public static final String KAFKA_CLIENT_PID = "org.opennms.features.kafka.consumer.client";
- 
-     private final ThreadFactory threadFactory = new ThreadFactoryBuilder()
-             .setNameFormat("kafka-consumer-events-%d")
-             .build();
- 
-     private final ExecutorService executorService = Executors.newSingleThreadExecutor(threadFactory);
- 
-     public OpenNMSKafkaConsumer(ConfigurationAdmin configAdmin, EventForwarder eventForwarder) {
-         this.configAdmin = configAdmin;
-         this.eventForwarder = eventForwarder;
-     }
- 
-     public void init() throws IOException {
-         Properties consumerConfig = new Properties();
-         // Add default group as OpenNMS Instance ID, allow it be override from client properties.
-         consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, SystemInfoUtils.getInstanceId());
-         final Dictionary<String, Object> properties = configAdmin.getConfiguration(KAFKA_CLIENT_PID).getProperties();
-         if (properties != null) {
-             final Enumeration<String> keys = properties.keys();
-             while (keys.hasMoreElements()) {
-                 final String key = keys.nextElement();
-                 consumerConfig.put(key, properties.get(key));
-             }
-         }
-         // Overwrite deserializer
-         consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
-         consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getCanonicalName());
-         KafkaConsumerRunner kafkaConsumerRunner = new KafkaConsumerRunner(consumerConfig);
-         executorService.execute(kafkaConsumerRunner);
-     }
- 
-     public void shutdown() {
-         closed.set(true);
-         executorService.shutdown();
-     }
- 
-     public void setEventsTopic(String eventsTopic) {
-         this.eventsTopic = eventsTopic;
-     }
- 
- 
-     private class KafkaConsumerRunner implements Runnable {
- 
-         private final Properties consumerConfig;
- 
-         public KafkaConsumerRunner(Properties properties) {
-             consumerConfig = properties;
-         }
- 
-         @Override
-         public void run() {
-             KafkaConsumer<String, byte[]> consumer = Utils.runWithGivenClassLoader(() -> new KafkaConsumer<>(consumerConfig), KafkaConsumer.class.getClassLoader());
-             if (Strings.isNullOrEmpty(eventsTopic)) {
-                 LOG.error("EventsTopic is either null or empty, not invoking kafka consumer");
-                 return;
-             }
-             consumer.subscribe(Arrays.asList(eventsTopic));
-             LOG.info("subscribed to {}", eventsTopic);
-             while (!closed.get()) {
-                 ConsumerRecords<String, byte[]> records = consumer.poll(java.time.Duration.ofMillis(Long.MAX_VALUE));
-                 LOG.trace("Received {} records", records.count());
-                 List<EventsProto.Event> pbEvents = new ArrayList<>();
-                 for (ConsumerRecord<String, byte[]> record : records) {
-                     try {
-                         EventsProto.Event pbEvent = EventsProto.Event.parseFrom(record.value());
-                         pbEvents.add(pbEvent);
-                     } catch (InvalidProtocolBufferException e) {
-                         LOG.warn("Error while parsing event with key {}", record.key());
-                     }
-                 }
-                 forwardEventsToOpenNMS(pbEvents);
-             }
-         }
- 
-         void forwardEventsToOpenNMS(List<EventsProto.Event> pbEvents) {
-             List<Event> events = EventsMapper.mapProtobufToEvents(pbEvents);
-             Log log = new Log();
-             events.forEach(log::addEvent);
-             // SendNowSync sends events in synchronous mode which puts more backpressure on Kafka.
-             eventForwarder.sendNowSync(log);
-             log.getEvents().getEventCollection().forEach(event -> {
-                 LOG.debug(" Event with uei {}, source {} forwarded to OpenNMS", event.getUei(), event.getSource());
-             });
-         }
- 
-     }
+// import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
 
-     private class XmlKafkaConsumerRunner implements Runnable {
- 
-        private final Properties consumerConfig;
+//import org.w3c.dom.Document;
+import Mapper.EventsMapper;
 
-        public XmlKafkaConsumerRunner(Properties properties) {
-            consumerConfig = properties;
-        }
+// import java.io.IOException;
+// import java.io.OutputStream;
+// import java.net.Socket;
 
-        @Override
-        public void run() {
-            KafkaConsumer<String, String> consumer = Utils.runWithGivenClassLoader(() -> new KafkaConsumer<>(consumerConfig), KafkaConsumer.class.getClassLoader());
-            if (Strings.isNullOrEmpty(eventsTopic)) {
-                LOG.error("EventsTopic is either null or empty, not invoking kafka consumer");
-                return;
-            }
-            consumer.subscribe(Arrays.asList(eventsTopic));
-            LOG.info("subscribed to {}", eventsTopic);
-            while (!closed.get()) {
-                ConsumerRecords<String, String> records = consumer.poll(java.time.Duration.ofMillis(Long.MAX_VALUE));
-                LOG.trace("Received {} records", records.count());
-                List<EventsProto.Event> pbEvents = new ArrayList<>();
-                for (ConsumerRecord<String, String> record : records) {
-                    try {
-                        EventsProto.Event pbEvent = EventsProto.Event.parseFrom(record.value());
-                        pbEvents.add(pbEvent);
-                    } catch (InvalidProtocolBufferException e) {
-                        LOG.warn("Error while parsing event with key {}", record.key());
-                    }
+public class Consumer{
+    private static final Logger log = LoggerFactory.getLogger(Consumer.class);
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer");
+
+        String bootstrapServers = "192.168.1.30:9092";
+        String groupId = "my-fifth-application";
+        String topic = "opennms-kafka-events";
+        
+        // String host = "192.168.1.30";
+        // int port = 5817;
+
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        //properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        //properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Create a Kafka consumer instance for protobuf messages
+        KafkaConsumer<String, byte[]> protobufConsumer = new KafkaConsumer<>(properties);
+        
+        // Create a Kafka consumer instance for XML messages
+        KafkaConsumer<String, String> xmlConsumer = new KafkaConsumer<>(properties);
+        
+        // get a reference to the current thread
+        final Thread mainThread = Thread.currentThread();
+
+        // adding the shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Detected a shutdown, let's exit by calling consumer.wakeup()...");
+                protobufConsumer.wakeup();
+                xmlConsumer.wakeup();
+
+                // join the main thread to allow the execution of the code in the main thread
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                forwardEventsToOpenNMS(pbEvents);
             }
-        }
+        });
 
-        void forwardEventsToOpenNMS(List<EventsProto.Event> pbEvents) {
-            List<Event> events = EventsMapper.mapProtobufToEvents(pbEvents);
-            Log log = new Log();
-            events.forEach(log::addEvent);
-            // SendNowSync sends events in synchronous mode which puts more backpressure on Kafka.
-            eventForwarder.sendNowSync(log);
-            log.getEvents().getEventCollection().forEach(event -> {
-                LOG.debug(" Event with uei {}, source {} forwarded to OpenNMS", event.getUei(), event.getSource());
-            });
+        try {
+            // subscribe consumers to the topic(s)
+            protobufConsumer.subscribe(Arrays.asList("protobuf-topic"));
+            xmlConsumer.subscribe(Arrays.asList("opennms-kafka-events", "outro xml topic"));
+
+            log.info("subscribed to {}", topic);
+
+            //*****Thread Safety***** https://www.oreilly.com/library/view/kafka-the-definitive/9781491936153/ch04.html
+            // You can’t have multiple consumers that belong to the same group in one thread and you can’t have multiple threads safely use the same consumer. 
+            // One consumer per thread is the rule. To run multiple consumers in the same group in one application, you will need to run each in its own thread.
+            // It is useful to wrap the consumer logic in its own object and then use Java’s ExecutorService to start multiple threads each with its own consumer.
+            // The Confluent blog has a tutorial that shows how to do just that.
+
+            // poll for new data
+            while (true) {
+                // Poll for protobuf messages
+                ConsumerRecords<String, byte[]> protobufRecords = protobufConsumer.poll(java.time.Duration.ofMillis(Long.MAX_VALUE));
+                for (ConsumerRecord<String, byte[]> record : protobufRecords) {
+                    // Handle protobuf messages
+                    // ...
+                    // try {
+                        // EventsProto.Event pbEvent = EventsProto.Event.parseFrom(record.value());
+                        // pbEvents.add(pbEvent);
+                    // } catch (InvalidProtocolBufferException e) {
+                        // LOG.warn("Error while parsing event with key {}", record.key());
+                    // }
+                }
+
+                // Poll for XML messages
+                ConsumerRecords<String, String> xmlRecords = xmlConsumer.poll(java.time.Duration.ofMillis(Long.MAX_VALUE));
+                for (ConsumerRecord<String, String> record : xmlRecords) {
+                    // Handle XML messages
+                    // ...
+                }
+                
+                //send event
+                //forwardEventsToOpenNMS(pbEvents);
+            }    
+
+        } catch (WakeupException e) {
+            log.info("Wake up exception!");
+            // we ignore this as this is an expected exception when closing a consumer
+        } catch (Exception e) {
+            log.error("Unexpected exception", e);
+        } finally {
+            protobufConsumer.close(); // this will also commit the offsets if need be.
+            xmlConsumer.close();
+            log.info("The consumer is now gracefully closed.");
         }
 
     }
- }
+
+    // void forwardEventsToOpenNMS(List<EventsProto.Event> pbEvents) {
+    //     List<Event> events = EventsMapper.mapProtobufToEvents(pbEvents);
+    //     Log log = new Log();
+    //     events.forEach(log::addEvent);
+    //     // SendNowSync sends events in synchronous mode which puts more backpressure on Kafka.
+    //     eventForwarder.sendNowSync(log);
+    //     log.getEvents().getEventCollection().forEach(event -> {
+    //         LOG.debug(" Event with uei {}, source {} forwarded to OpenNMS", event.getUei(), event.getSource());
+    //     });
+    // }
+
+}
+
+
+
+
+
  
