bd33a5e45cc45bdaa0b1fb54fe348b152f0e987d
Eduardo Pousa <92334531+epousa@users.noreply.github.com>
2023-03-18 21:35:13 +0000

    Updated Consumer_1

--- cenarios-teste/monitoring-stack/config/kafka/test-consumer/src/Consumer.java
+++ cenarios-teste/monitoring-stack/config/kafka/test-consumer/src/Consumer.java
@@ -1,3 +1,5 @@
+package Main;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 //import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -10,21 +12,24 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 // import java.time.Duration;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
-import java.io.StringReader;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
+
+
+import Mapper.EventsMapper;
 
 // import java.io.IOException;
 // import java.io.OutputStream;
 // import java.net.Socket;
 
-public class Consumer {
+public class Consumer{
     private static final Logger log = LoggerFactory.getLogger(Consumer.class);
 
     public static void main(String[] args) {
@@ -68,7 +73,6 @@ public class Consumer {
         });
 
         try {
-
             //Para o caso de ter topicos que tem eventos com configurações 
             //diferentes tentar criar uma thread por cada topico. 
             //Cada thread usa um eventmapper different
@@ -82,30 +86,50 @@ public class Consumer {
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(java.time.Duration.ofMillis(Long.MAX_VALUE));
                 log.info("Received {} records", records.count());
+
+                //List<Document> xmlEvents = new ArrayList<>();
+                //List<EventsProto.Event> pbEvents = new ArrayList<>();
+
+                Map<String, List<Document>> xmlEventsByTopic = new HashMap<>();
+
                 for(ConsumerRecord<String, String> record:records){
                     System.out.println(record.value());
                     
                     //manipulate event
                     switch(record.topic()){
                         case "opennms-kafka-events":
+                        case "outro xml topic":
                             //xml topic type-1 
                             System.out.println("EventsMapper for xml");
-                            Document doc = convertStringToDocument(record.value());
-                            String uei = doc.getElementsByTagName("uei").item(0).getTextContent();
-                            System.out.println(uei);
 
+                            xmlEventsByTopic.computeIfAbsent(record.topic(), k -> new ArrayList<>()).add(EventsMapper.parseFrom(record.value()));
+
+                            // Document doc = EventsMapper.parseFrom(record.value());
+                            // xmlEvents.add(doc);
+                            
                             break;
+
                         default:
-                            //protobuf topic
+                            // protobuf topic
+
+                            // try {
+                            //     EventsProto.Event pbEvent = EventsProto.Event.parseFrom(record.value());
+                            //     pbEvents.add(pbEvent);
+                            // } catch (InvalidProtocolBufferException e) {
+                            //     LOG.warn("Error while parsing event with key {}", record.key());
+                            // }
+
                             System.out.println("EventsMapper for protobuf");
                             byte[] byteArrray = record.value().getBytes();
                             System.out.println(byteArrray);
+
+                           
 
                             break;
                     }
                 }
                 //send event
-                // orwardEventsToOpenNMS(pbEvents);
+                //forwardEventsToOpenNMS(pbEvents);
             }
 
         } catch (WakeupException e) {
@@ -120,19 +144,16 @@ public class Consumer {
 
     }
 
-    private static Document convertStringToDocument(String xmlStr) {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
-        DocumentBuilder builder;  
-        try  
-        {  
-            builder = factory.newDocumentBuilder();  
-            Document doc = builder.parse( new InputSource( new StringReader( xmlStr ) ) ); 
-            return doc;
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        } 
-        return null;
-    }
+    // void forwardEventsToOpenNMS(List<EventsProto.Event> pbEvents) {
+    //     List<Event> events = EventsMapper.mapProtobufToEvents(pbEvents);
+    //     Log log = new Log();
+    //     events.forEach(log::addEvent);
+    //     // SendNowSync sends events in synchronous mode which puts more backpressure on Kafka.
+    //     eventForwarder.sendNowSync(log);
+    //     log.getEvents().getEventCollection().forEach(event -> {
+    //         LOG.debug(" Event with uei {}, source {} forwarded to OpenNMS", event.getUei(), event.getSource());
+    //     });
+    // }
 
 }
 
